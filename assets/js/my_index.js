ae8f600975226db7b5705f60cfd4f47dc3b341d7
RGondan <gondan.ga2@gmail.com>
2024-05-30 00:15:38 -0400

    Se agrega y adapta para que funcione con base proveida por Desafio Latam

--- assets/js/my_index.js
+++ assets/js/my_index.js
@@ -1,10 +1,15 @@
-precio = 400000
+const precio = 400000
+let cantidad = 0
 
-precioSpan = document.querySelector(".precio-inicial");
-precioSpan.innerHTML = precio
+document.onload = init();
+
+function init() {
+    document.querySelector("#amount").innerText = cantidad
+    document.querySelector("#price").innerText = precio
+}
 
 function minusAmount(){
-    const element =  document.querySelector(".cantidad")
+    const element =  document.querySelector("#amount")
     let amount = parseInt(element.innerText);
     amount -= 1;
     if (amount < 0) {
@@ -15,7 +20,7 @@ function minusAmount(){
 }
 
 function plusAmount(){
-    const element =  document.querySelector(".cantidad")
+    const element =  document.querySelector("#amount")
     let amount = parseInt(element.innerText);
     amount += 1;
     element.innerText = amount;
@@ -23,12 +28,12 @@ function plusAmount(){
 }
 
 function calculateTotal(){
-    const amount_element =  document.querySelector(".cantidad")
+    const amount_element =  document.querySelector("#amount")
     let amount = parseInt(amount_element.innerText);
-    let price_element =  document.querySelector(".precio-inicial")
+    let price_element =  document.querySelector("#price")
     let price = parseInt(price_element.innerText);
 
     var total = price * amount;
-    const total_element =  document.querySelector(".valor-total")
+    const total_element =  document.querySelector("#total")
     total_element.innerText = total;
 }
