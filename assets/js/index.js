fd172648b92a0094a5509b1000c8ba4b8f39b745
RGondan <gondan.ga2@gmail.com>
2024-05-29 22:51:12 -0400

    Se cambian var a let o const

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,6 @@
 function minusAmount(){
-    var element =  document.querySelector("#amount")
-    var amount = parseInt(element.innerText);
+    const element =  document.querySelector("#amount")
+    let amount = parseInt(element.innerText);
     amount -= 1;
     if (amount < 0) {
         amount = 0;
@@ -10,20 +10,20 @@ function minusAmount(){
 }
 
 function plusAmount(){
-    var element =  document.querySelector("#amount")
-    var amount = parseInt(element.innerText);
+    const element =  document.querySelector("#amount")
+    let amount = parseInt(element.innerText);
     amount += 1;
     element.innerText = amount;
     calculateTotal()
 }
 
 function calculateTotal(){
-    var amount_element =  document.querySelector("#amount")
-    var amount = parseInt(amount_element.innerText);
-    var price_element =  document.querySelector("#price")
-    var price = parseInt(price_element.innerText);
+    const amount_element =  document.querySelector("#amount")
+    let amount = parseInt(amount_element.innerText);
+    let price_element =  document.querySelector("#price")
+    let price = parseInt(price_element.innerText);
 
     var total = price * amount;
-    var total_element =  document.querySelector("#total")
+    const total_element =  document.querySelector("#total")
     total_element.innerText = total;
 }
